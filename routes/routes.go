4f53307ccd4e693c1903b9e1fa2d20c5302369ef
Jadson Medeiros <developer.medeiros@gmail.com>
2022-01-19 21:59:09 -0300

    added router

--- routes/routes.go
+++ routes/routes.go
@@ -4,11 +4,13 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/jadson-medeiros/go-rest/controllers"
 )
 
 func HandleRequest() {
-	http.HandleFunc("/", controllers.Home)
-	http.HandleFunc("/api/personalities", controllers.GetAllPersonalities)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	router := mux.NewRouter()
+	router.HandleFunc("/", controllers.Home)
+	router.HandleFunc("/api/personalities", controllers.GetAllPersonalities)
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
