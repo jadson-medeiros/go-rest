20546c28b0cc26faf652ea5a0d836794671b9aff
Jadson Medeiros <developer.medeiros@gmail.com>
2022-01-25 16:36:25 -0300

    added middleware

--- routes/routes.go
+++ routes/routes.go
@@ -6,10 +6,13 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/jadson-medeiros/go-rest/controllers"
+	"github.com/jadson-medeiros/go-rest/middleware"
 )
 
 func HandleRequest() {
 	router := mux.NewRouter()
+	router.Use(middleware.ContetTypeMiddleware)
+
 	router.HandleFunc("/", controllers.Home)
 	router.HandleFunc("/api/personalities", controllers.GetAllPersonalities).Methods("Get")
 	router.HandleFunc("/api/personalities/{id}", controllers.GetPersonalityById).Methods("Get")
