370f60e897a4f05a37ea30b4361d2b80215e35a7
Jadson Medeiros <developer.medeiros@gmail.com>
2022-01-25 20:22:18 -0300

    added CORS

--- routes/routes.go
+++ routes/routes.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/jadson-medeiros/go-rest/controllers"
 	"github.com/jadson-medeiros/go-rest/middleware"
@@ -19,5 +20,5 @@ func HandleRequest() {
 	router.HandleFunc("/api/personalities", controllers.CreatePersonality).Methods("Post")
 	router.HandleFunc("/api/personalities/{id}", controllers.DeletePersonality).Methods("Delete")
 	router.HandleFunc("/api/personalities/{id}", controllers.EditPersonality).Methods("Put")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(router)))
 }
